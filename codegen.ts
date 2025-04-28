040df3f1f54087a5ead7d646ffbd61bb18631fcc
Furkan Abay <furkan_abay@hotmail.com>
2025-04-28 02:38:15 +0200

    feat: minor UI changes on error&loading page

--- codegen.ts
+++ codegen.ts
@@ -1,5 +1,3 @@
-
-import 'dotenv/config';
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
--- src/components/atoms/ErrorPage.tsx
+++ src/components/atoms/ErrorPage.tsx
@@ -2,15 +2,17 @@ import MainLink from "./buttons/MainLink";
 
 export default function ErrorPage() {
   return (
-    <section className="flex flex-col mt-20 md:mt-24 max-w-[90%]">
-      <h4 className="text-ds-dark font-normal md:font-light text-xl max-w-[900px] animate-pulse pointer-events-none
+    <main className="max-w-[1200px] pb-8 px-6 md:px-24 mx-auto">
+      <section className="flex flex-col mt-20 md:mt-24 max-w-[90%]">
+        <h4 className="text-ds-dark font-normal md:font-light text-xl max-w-[900px] animate-pulse pointer-events-none
       sm:text-2xl
       md:text-6xl">
-        We're currently experiencing issues & the service is down. Please check back a bit later :(
-      </h4>
-      <MainLink href={"/"}>
-        Go back to homepage
-      </MainLink>
-    </section>
+          We're currently experiencing issues & the service is down. Please check back a bit later :(
+        </h4>
+        <MainLink href={"/"}>
+          Go back to homepage
+        </MainLink>
+      </section>
+    </main>
   )
 }
